c0c30782784ebe281e3ee55eceffd2040d58b87a
S Soundarya <soundarya.s@tvsnext.com>
2023-07-14 14:52:27 +0530

    [Soundarya]Add:initial commit.

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical programs: ");
+        }
+    }
+
+}
