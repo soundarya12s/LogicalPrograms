7faa35af77da839d41660192a74dccb47ac7930a
S Soundarya <soundarya.s@tvsnext.com>
2023-07-14 15:37:57 +0530

    [Soundarya]Add: Fibonacci.

---
+++ LogicalPrograms/Fibonacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Fibonacci
+    {
+        public void FibonacciSeries()
+        {
+            int a = 0, b = 1;
+            int c = a + b;
+            Console.WriteLine("Enter the number");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fibonacci Series : " + a + " " + b + " ");
+            for (int i = 3; i <= limit; i++)
+            {
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+                c = a + b;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using LogicalPrograms;
 
 namespace LineComparisonProgram
 {
@@ -6,7 +6,22 @@ namespace LineComparisonProgram
     {
         static void Main(string[] args)
         {
+            Fibonacci f = new Fibonacci();
+           
+
+
             Console.WriteLine("Logical programs: ");
+            Console.WriteLine("\n1.Fibonacci Series ");
+
+            int count = Convert.ToInt32(Console.ReadLine());
+            
+            switch (count)
+            {
+                case 1:
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    f.FibonacciSeries();
+                    break;
+            }
         }
     }
 
